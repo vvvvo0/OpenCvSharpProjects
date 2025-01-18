2d4bb92c2b00015a96eeffaded9c347d6927ef21
vvvvo0 <idvvvvo@gmail.com>
2025-01-18 14:18:52 +0900

    웹캠으로 화면을 실시간으로 분석하고 특정 객체를 인식하여 특정 행동을 하도록 하는 프로그램(1) 웹캠 화면을 표시하고 간단한 이미지 처리

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,14 @@
-﻿using System.Windows.Threading;
+﻿
+using System.Windows.Threading;
 
 using OpenCvSharp;
 // OpenCV4의 데이터 형식이나 함수 및 메서드를 사용하기 위해 네임스페이스에 using OpenCvSharp;을 추가합니다.
 // Mat 클래스 또한 using OpenCvSharp;에 포함되어 있습니다.
+
+using OpenCvSharp.WpfExtensions;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
 
 namespace OpenCvSharpProjects
 {
@@ -13,7 +19,11 @@ namespace OpenCvSharpProjects
     {
         private VideoCapture capture; // VideoCapture 객체를 저장할 필드, 웹캠에서 영상을 가져오는 데 사용된다
         private Mat frame; // Mat 객체를 저장할 필드, OpenCV에서 이미지를 표현하는 데 사용된다
-        private DispatcherTimer timer; // DispatcherTimer 객체를 저장할 필드, UI 스레드에서 작업을 예약하는 데 사용된다
+        
+        
+        /* 
+        private DispatcherTimer timer; // DispatcherTimer 객체를 저장할 필드, UI 스레드에서 작업을 예약하는 데 사용된다 
+        */
 
         public MainWindow() // 생성자
         {
@@ -21,30 +31,67 @@ namespace OpenCvSharpProjects
 
             capture = new VideoCapture(0); // VideoCapture 객체를 생성하고 기본 웹캠 장치(0)를 사용하도록 설정한다
             frame = new Mat(); // Mat 객체를 생성한다
+
+
+            /*
             timer = new DispatcherTimer(); // DispatcherTimer 객체를 생성한다
             timer.Interval = TimeSpan.FromMilliseconds(33); // 타이머 간격을 33밀리초로 설정한다. 약 30fps(초당 프레임 수)로 프레임을 업데이트한다
             timer.Tick += UpdateFrame; // timer의 Tick 이벤트에 UpdateFrame 메서드를 연결한다. 즉, 타이머가 발생할 때마다 UpdateFrame 메서드가 호출된다
             timer.Start(); // 타이머를 시작한다
+            */
+
+
+            // CompositionTarget.Rendering 이벤트를 사용하여 프레임 업데이트
+            CompositionTarget.Rendering += UpdateFrame; 
         }
 
-        private void UpdateFrame(object sender, EventArgs e) // 타이머 이벤트가 발생할 때마다 호출되는 메서드이다
+        private void UpdateFrame(object? sender, EventArgs e) // 타이머 이벤트가 발생할 때마다 호출되는 메서드이다
         {
-            capture.Read(frame); // 웹캠에서 현재 프레임을 읽어와 frame 객체에 저장한다
-            if (!frame.Empty()) // frame 객체가 비어 있지 않으면, 즉 프레임을 읽어오는 데 성공하면 다음 코드를 실행한다
+            capture.Read(frame); // 웹캠에서 현재 프레임을 읽어와, frame 객체에 저장한다
+
+
+            if (!frame.Empty()) // frame 객체가 비어 있지 않으면(프레임을 읽어오는 데 성공하면), 다음 코드를 실행한다
             {
+                /*
                 Cv2.ImShow("Camera Output", frame); // Cv2.ImShow() 메서드를 사용하여 frame 객체(이미지)를 "Camera Output" 창(OpenCV 창)에 표시한다
+                */
+
+                // 이미지 처리 및 객체 인식 수행
+                ProcessImage(frame); 
+
+
+                // 화면에 이미지 표시
+                Dispatcher.Invoke(() =>
+                {
+                    // WriteableBitmap을 사용하여 이미지 표시
+                    CameraImage.Source = WriteableBitmapConverter.ToWriteableBitmap(frame);
+                });
+
             }
         }
+
+
+        private void ProcessImage(Mat image)
+        {
+            // TODO: 게임 화면 영역 검출, 특정 객체 인식, 행동 결정 등의 로직 구현
+
+            // 예시: 이미지에 빨간색 사각형 그리기
+            Cv2.Rectangle(image, new Rect(100, 100, 200, 100), Scalar.Red, 2);
+        }
+
 
 
         // 윈도우가 닫힐 때 리소스를 해제하는 방법
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) // 윈도우가 닫힐 때 발생하는 이벤트를 처리하는 메서드이다
         {
-            // 윈도우가 닫힐 때 리소스를 해제합니다.
+            // 윈도우가 닫힐 때 리소스를 해제한다
             capture.Release(); // VideoCapture 객체를 해제하여 웹캠 장치를 닫는다
             frame.Dispose(); // Mat 객체를 해제하여 이미지 데이터가 차지하는 메모리를 해제한다
             Cv2.DestroyAllWindows(); // 모든 OpenCV 창을 닫는다
+            
+            /*
             timer.Stop(); // 타이머를 중지한다
+            */
         }
         
     }
