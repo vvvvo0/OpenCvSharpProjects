9fe7770393ab50eb43f24b7f08fdbd3ad8f334f8
vvvvo0 <idvvvvo@gmail.com>
2025-01-27 01:49:11 +0900

    MVVM 패턴 적용 (4) Views 폴더, RectToVisibilityConverter 컨버터 구현

---
+++ Converters/RectToThicknessConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OpenCvSharpProjects.Converters
+{
+    public class RectToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Rect rect)
+            {
+                return new Thickness(rect.Left, rect.Top, 0, 0); // 왼쪽, 위쪽 여백을 Rect의 Left, Top 값으로 설정하고, 오른쪽, 아래쪽 여백은 0으로 설정합니다.
+            }
+            return new Thickness(0); // 기본값으로 0을 반환합니다.
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // ConvertBack 메서드는 구현하지 않습니다.
+        }
+    }
+}
--- Converters/RectToVisibilityConverter.cs
+++ Converters/RectToVisibilityConverter.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
 
+
+// Rect 객체를 Visibility (UI 요소의 표시 여부) 값으로 변환하는 컨버터
 namespace OpenCvSharpProjects.Converters
 {
-    internal class RectToVisibilityConverter
+    internal class RectToVisibilityConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Rect rect && !rect.IsEmpty)
+            {
+                return Visibility.Visible; // Rect 객체가 비어있지 않으면 Visible을 반환한다.
+            }
+            else
+            {
+                return Visibility.Collapsed; // Rect 객체가 비어있으면 Collapsed를 반환한다.
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // ConvertBack 메서드는 구현하지 않는다.
+        }
     }
 }
